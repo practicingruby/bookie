474cf3f8c667ad0c435e40b9a0c20ef101ea432c
Gregory Brown <gregory.t.brown@gmail.com>
2011-04-23 21:24:38 -0400

    Introduce pargraph objects, but this API sucks, right?

--- lib/bookie/parser.rb
+++ lib/bookie/parser.rb
@@ -1,22 +1,30 @@
 module Bookie
   ContentTree = Struct.new(:children)
-
+  Paragraph   = Struct.new(:children)
+  
   class Parser
     def self.parse(contents)
       parser = new(contents)
       parser.document_tree
     end
 
+    attr_reader :document_tree
+
     def initialize(contents)
       generate_document_tree(contents)
     end
 
-    attr_reader :document_tree
+    def extract_paragraphs(contents)
+      contents.split(/\n\n+/).each do |e|
+        document_tree.children << Paragraph.new([e])
+      end
+    end
 
     private
 
     def generate_document_tree(contents)
-      @document_tree = ContentTree.new(contents.split(/\n\n+/))
+      @document_tree = ContentTree.new([])
+      extract_paragraphs(contents)
     end
   end
 end
--- test/units/parser_test.rb
+++ test/units/parser_test.rb
@@ -2,15 +2,18 @@ require_relative "../test_helper"
 
 context "A Parser" do
   test "should know about paragraphs" do
-    sample_text      = File.read(fixture("multi_paragraph_document.md"))
+    sample_text           = File.read(fixture("multi_paragraph_document.md"))
 
     # NOTE: Is this the behavior we'd expect?
-    sample_paragraph = File.read(fixture("single_paragraph.md")).chomp
+    sample_paragraph_text = File.read(fixture("single_paragraph.md")).chomp
 
     tree = Bookie::Parser.parse(sample_text)
    
     assert_equal 8, tree.children.length
 
-    assert_equal sample_paragraph, tree.children[4]
+    actual_paragraph      = tree.children[4]
+    actual_paragraph_text = actual_paragraph.children.first
+
+    assert_equal sample_paragraph_text, actual_paragraph_text 
   end
 end
