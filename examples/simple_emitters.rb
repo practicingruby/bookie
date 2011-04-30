49e115e1524987c657018646f8dd932b96cf58eb
Gregory Brown <gregory.t.brown@gmail.com>
2011-04-30 17:03:07 -0400

    Remove examples, because the API is complete trash

--- examples/simple_emitters.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/bookie"
-Prawn.debug = true
-
-file = "#{File.dirname(__FILE__)}/../test/fixtures/multi_paragraph_document.md"
-
-pdf_document = Bookie::Document.new(file, Bookie::Emitters::PDF.new(
-header: "Majestic Sea Creature Blog", title:  "Why does RbMU exist?"))
-
-pdf_document.render(file: "output.pdf")
